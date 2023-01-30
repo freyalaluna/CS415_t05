86c84e7207866d18d6bd954e990de2ae422bbae1
b-ju <ben.jurenka@colostate.edu>
2023-01-30 07:35:05 -0700

    hashcode tests

--- server/src/test/java/edu/colostate/cs415/model/WorkerTest.java
+++ server/src/test/java/edu/colostate/cs415/model/WorkerTest.java
@@ -1,10 +1,36 @@
 package edu.colostate.cs415.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class WorkerTest {
+    private Worker worker;
+
+    @Before
+    public void setUp() throws Exception {
+		Set<Qualification> qualifications = new HashSet<Qualification>();
+        worker = new Worker("", qualifications, 0.0);
+    }
+
 	@Test
-	public void test() {
-		assert (true);
-	}
+    public void testHashCodeWithNullString() {
+        assertEquals( "Worker.hashCode returns 0 with a null string", worker.hashCode(), 0);
+    }
+
+	@Test
+    public void testHashCodeWithEmptyString() {
+        assertEquals( "Worker.hashCode returns 0 with an empty string", worker.hashCode(), 0);
+    }
+
+	@Test
+    public void testHashCodeWithValidString() {
+		Worker workerWithValidName = new Worker(("test"), null, 0);
+        assertThat("Worker.hashCode returns a non 0 code with a valid name", workerWithValidName.hashCode(), is(not(0)));
+    }
+
 }
