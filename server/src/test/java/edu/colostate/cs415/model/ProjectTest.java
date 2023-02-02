f185fd2b01d468c587abd6e9d5b710d055c7506f
Elita Danilyuk <elitadanilyuk@gmail.com>
2023-02-02 12:59:51 -0700

    adding before tests for project constructor to set up a project for test cases

--- server/src/test/java/edu/colostate/cs415/model/ProjectTest.java
+++ server/src/test/java/edu/colostate/cs415/model/ProjectTest.java
@@ -1,10 +1,25 @@
 package edu.colostate.cs415.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Test;
+import org.junit.Before;
 
 public class ProjectTest {
+
+	private Project project;
+	private ProjectSize size;
+
+	@Before
+    public void setUp() throws Exception {
+        Set<Qualification> qualifications = new HashSet<Qualification>();
+        project = new Project("", qualifications, size);
+    }
+
+
 	@Test
-	public void test() {
+	public void testProjectConstruct() {
 		assert (true);
 	}
 }
