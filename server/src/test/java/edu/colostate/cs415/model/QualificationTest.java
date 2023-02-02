e1e2c86d645dc8bca3dcf3698b09479c50332bcf
Marti Selig <mselig@colostate.edu>
2023-02-02 13:09:20 -0700

    Added tests for Qualification hashCode()

--- server/src/test/java/edu/colostate/cs415/model/QualificationTest.java
+++ server/src/test/java/edu/colostate/cs415/model/QualificationTest.java
@@ -1,10 +1,26 @@
 package edu.colostate.cs415.model;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class QualificationTest {
+	/***** HASHCODE *****/
 	@Test
-	public void test() {
-		assert (true);
+	public void testHashCodeWithNullString() {
+		Qualification qualificationWithNullDesc = new Qualification("");
+		assertEquals("Qualification.hashCode returns 0 with a null string", qualificationWithNullDesc.hashCode(), 0);
+	}
+
+	@Test
+	public void testHashCodeWithEmptyString() {
+		Qualification qualificationWithEmptyDesc = new Qualification("");
+		assertEquals( "Qualification.hashCode returns 0 with an empty string", qualificationWithEmptyDesc.hashCode(), 0);
+	}
+
+	@Test
+	public void testHashCodeWithValidString() {
+		Qualification qualificationWithValidDesc = new Qualification("test");
+		assertThat("Qualification.hashCode returns a non 0 code with a valid name", qualificationWithValidDesc.hashCode(), is(not(0)));
 	}
 }
