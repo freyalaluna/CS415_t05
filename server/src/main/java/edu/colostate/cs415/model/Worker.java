316289e29adfef3e56acc4e687fc9845edfa4a77
nchaffee0 <nchaffee@colostate.edu>
2023-02-11 02:39:50 -0700

    changed value to MAX_WORKLOAD variable

--- server/src/main/java/edu/colostate/cs415/model/Worker.java
+++ server/src/main/java/edu/colostate/cs415/model/Worker.java
@@ -104,7 +104,7 @@ public class Worker {
 		if(getProjects().contains(project)){
 			return getWorkload() > MAX_WORKLOAD ? true : false;
 		}
-		return (project.getSize().getValue() + getWorkload()) > 12 ? true : false;
+		return (project.getSize().getValue() + getWorkload()) > MAX_WORKLOAD ? true : false;
 	}
 
 	public boolean isAvailable() {
