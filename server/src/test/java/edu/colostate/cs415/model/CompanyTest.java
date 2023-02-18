c5a69196bcfbaf30b5f64c851b0c7a05fe7f67c1
b-ju <ben.jurenka@colostate.edu>
2023-02-18 13:17:13 -0700

    test company constructor

--- server/src/test/java/edu/colostate/cs415/model/CompanyTest.java
+++ server/src/test/java/edu/colostate/cs415/model/CompanyTest.java
@@ -1,10 +1,33 @@
 package edu.colostate.cs415.model;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CompanyTest {
+
+	private Company company;
+
+	@Rule public ExpectedException thrown = ExpectedException.none();
+
 	@Test
-	public void test() {
-		assert (true);
+	public void testCompanyConstructor() {
+		// add assert name when getName is done
+		company = new Company("test co");
+		// assertEquals("test co", company.getName());
+	}
+
+	@Test
+	public void testCompanyConstructorThrowsExceptionWithNullName() {
+		thrown.expect(IllegalArgumentException.class);
+		company = new Company(null);
+	}
+
+	@Test
+	public void testCompanyConstructorThrowsExceptionWithEmptyName() {
+		thrown.expect(IllegalArgumentException.class);
+		company = new Company("");
 	}
 }
