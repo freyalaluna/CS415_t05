5337f3f9cad02e630a0ed82a79cb813f5bcd947f
Marti Selig <mselig@colostate.edu>
2023-04-12 18:38:05 -0600

    testPutFinish3

--- server/src/test/java/edu/colostate/cs415/server/RestControllerTest.java
+++ server/src/test/java/edu/colostate/cs415/server/RestControllerTest.java
@@ -629,4 +629,23 @@ public class RestControllerTest {
         .bodyByteArray(body.getBytes())
         .execute().returnContent();
     }
+
+    @Test
+    public void testPutFinish3() throws IOException {
+        // null project
+        company = new Company("Company 1");
+        Qualification java = company.createQualification("Java");
+        Set<Qualification> quals = new HashSet<Qualification>();
+        quals.add(java);
+        Worker w1 = company.createWorker("w", quals, 10);
+        Project p1 = company.createProject("Moon mission", quals, ProjectSize.SMALL);
+        company.assign(w1, p1);
+        company.start(p1);
+        String body = "{ \"name\": null}";
+        thrown.expect(HttpResponseException.class);
+        restController.start();
+        Request.put("http://localhost:4567/api/finish")
+        .bodyByteArray(body.getBytes())
+        .execute().returnContent();
+    }
 }
