dc6737bb53b7a31d115290b51fdc98f324022c3f
b-ju <ben.jurenka@colostate.edu>
2023-04-15 09:20:38 -0600

    add list of workers

--- client/src/pages/workers.js
+++ client/src/pages/workers.js
@@ -1,11 +1,36 @@
-import { pageStyle } from '../utils/styles'
+import { useEffect, useState } from 'react'
+import ClickList from '../components/ClickList'
+import LocationID from '../utils/location'
+import { darkGrayContainerStyle, grayContainerStyle,pageStyle } from '../utils/styles'
+import { getWorkers } from '../services/dataService'
+
+const Worker = (Worker, active) => {
+    return (
+        <div>
+            <div>{Worker.name}</div>
+            {active === true ? WorkerBody(Worker) : null}
+        </div>
+    )
+}
+
+const WorkerBody = (Worker) => {
+    return (
+        <div style={grayContainerStyle}>
+            Workers: <ClickList list={Worker.workers} styles={darkGrayContainerStyle} path="/workers" />
+        </div>
+    )
+}
 
 const Workers = () => {
+    const [workers, setworkers] = useState([])
+    useEffect(() => { getWorkers().then(setworkers) }, [])
+    const active = LocationID('workers', workers, 'description')
     return (
         <div style={pageStyle}>
             <h1>
                 This page displays a table containing all the workers.
             </h1>
+            <ClickList active={active} list={workers} item={Worker} path='/workers' id='name' />
         </div>
     )
 }
