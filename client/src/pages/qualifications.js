bbe79d804051eef7127f504aa235ce1aaae61b1a
b-ju <ben.jurenka@colostate.edu>
2023-04-15 09:30:17 -0600

    format

--- client/src/pages/qualifications.js
+++ client/src/pages/qualifications.js
@@ -22,33 +22,33 @@ const QualificationBody = (qualification) => {
 }
 
 const CreateQualificationForm = (props) => {
-    const {setQualifications} = props
+    const { setQualifications } = props
     return (
         <div className="card">
-        <div className="card-body">
-            <form>
-                <div className="form-group">
-                    <input type="text" className="form-control" id="qualDesc" placeholder="Enter a description" />
-                </div>
-                <button
-                    type="button" className="btn btn-outline-primary"
-                    onClick={() => {
-                        const description = document.getElementById('qualDesc').value
-                        if (description) {
-                            createQualification(description).then(response => {
-                                if (response?.data === 'OK') {
-                                    getQualifications().then(setQualifications)
-                                    document.getElementById('qualDesc').value = ""
-                                }
-                            })
-                        }
-                    }}
-                >
-                    Create a Qualification
-                </button>
-            </form>
+            <div className="card-body">
+                <form>
+                    <div className="form-group">
+                        <input type="text" className="form-control" id="qualDesc" placeholder="Enter a description" />
+                    </div>
+                    <button
+                        type="button" className="btn btn-outline-primary"
+                        onClick={() => {
+                            const description = document.getElementById('qualDesc').value
+                            if (description) {
+                                createQualification(description).then(response => {
+                                    if (response?.data === 'OK') {
+                                        getQualifications().then(setQualifications)
+                                        document.getElementById('qualDesc').value = ""
+                                    }
+                                })
+                            }
+                        }}
+                    >
+                        Create a Qualification
+                    </button>
+                </form>
+            </div>
         </div>
-    </div>
     )
 }
 
@@ -58,7 +58,7 @@ const Qualifications = () => {
     const active = LocationID('qualifications', qualifications, 'description')
     return (
         <div style={pageStyle}>
-            <CreateQualificationForm setQualifications={setQualifications}/>
+            <CreateQualificationForm setQualifications={setQualifications} />
             <br></br>
             <ClickList active={active} list={qualifications} item={Qualification} path='/qualifications' id='description' />
         </div>
