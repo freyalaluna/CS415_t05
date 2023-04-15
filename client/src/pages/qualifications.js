cd99a8872d173347daaf4d7f52392d69f0296373
b-ju <ben.jurenka@colostate.edu>
2023-04-15 09:29:29 -0600

    create form

--- client/src/pages/qualifications.js
+++ client/src/pages/qualifications.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import ClickList from '../components/ClickList'
-import { getQualifications } from '../services/dataService'
+import { createQualification, getQualifications } from '../services/dataService'
 import LocationID from '../utils/location'
 import { darkGrayContainerStyle, grayContainerStyle, pageStyle } from '../utils/styles'
 
@@ -21,15 +21,45 @@ const QualificationBody = (qualification) => {
     )
 }
 
+const CreateQualificationForm = (props) => {
+    const {setQualifications} = props
+    return (
+        <div className="card">
+        {/* <div className="card-body"> */}
+            <form>
+                <div className="form-group">
+                    <input type="text" className="form-control" id="qualDesc" placeholder="Enter a description" />
+                </div>
+                <button
+                    type="button" className="btn btn-outline-primary"
+                    onClick={() => {
+                        const description = document.getElementById('qualDesc').value
+                        // if (description) {
+                        //     createQualification(description).then(response => {
+                        //         if (response?.data === 'OK') {
+                        //             getQualifications().then(setQualifications)
+                        //             document.getElementById('qualDesc').value = ""
+                        //         }
+                        //     })
+                        // }
+                    }}
+                >
+                    Create a Qualification
+                </button>
+            </form>
+        {/* </div> */}
+    </div>
+    )
+}
+
 const Qualifications = () => {
     const [qualifications, setQualifications] = useState([])
     useEffect(() => { getQualifications().then(setQualifications) }, [])
     const active = LocationID('qualifications', qualifications, 'description')
     return (
         <div style={pageStyle}>
-            <h1>
-                This page displays a table containing all the qualifications.
-            </h1>
+            <CreateQualificationForm setQualifications={setQualifications}/>
+            <br></br>
             <ClickList active={active} list={qualifications} item={Qualification} path='/qualifications' id='description' />
         </div>
     )
