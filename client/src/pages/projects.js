3965cc8b387f058f4ad9f8453b18a178d447a0fd
Elita Danilyuk <elitadanilyuk@gmail.com>
2023-04-21 16:30:14 -0600

    adding projects list

--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -1,11 +1,37 @@
-import { pageStyle } from '../utils/styles'
+import { useEffect, useState } from 'react'
+import { darkGrayContainerStyle, grayContainerStyle, pageStyle } from '../utils/styles'
+import ClickList from '../components/ClickList'
+import LocationID from '../utils/location'
+import { getProjects } from '../services/dataService'
+
+const Project = (project, active) => {
+    return(
+        <div>
+            <div>{project.name}</div>
+            {active === true ? ProjectBody(project) : null}
+        </div>
+    )
+}
+
+const ProjectBody = (project) => {
+    return(
+        <div style={grayContainerStyle}>
+            Projects: <ClickList list={project} styles={darkGrayContainerStyle} path="/workers" />
+        </div>
+    )
+}
 
 const Projects = () => {
+    const [projects, setprojects] = useState([])
+    useEffect(() => { getProjects().then(setprojects) }, [])
+    const active = LocationID('projects', projects, 'name');
     return (
         <div style={pageStyle}>
             <h1>
-                This page displays a table containing all the projects.
+                This page displays all of the projects & will soon allow clicking to view project details.
             </h1>
+            <br/><br/>
+            <ClickList active={active} list={projects} item={Project} path='/projects' id='name'/>
         </div>
     )
 }
