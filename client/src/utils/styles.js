56c1664e43ffea1a6ea8b8eecda19bf46726c626
Elita Danilyuk <elitadanilyuk@gmail.com>
2023-04-24 22:55:40 -0600

    adding styling background for qualifications

--- client/src/utils/styles.js
+++ client/src/utils/styles.js
@@ -14,6 +14,14 @@ export const darkGrayContainerStyle = {
     background: '#CCCCCC',
 }
 
+export const missingStyle = {
+    background: 'red'
+}
+
+export const notMissingStyle = {
+    background: 'green'
+}
+
 export const pageStyle = {
     padding: '1vw',
 }
