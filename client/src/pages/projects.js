6bf6c439e71328d3cfea36b507f180ec6046a1f7
Elita Danilyuk <elitadanilyuk@gmail.com>
2023-05-01 15:29:53 -0600

    adding create new project form

--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
+
 import { darkGrayContainerStyle, grayContainerStyle, pageStyle, missingStyle, notMissingStyle } from '../utils/styles'
 import ClickList from '../components/ClickList'
 import LocationID from '../utils/location'
-import { getProjects, unasignWorker } from '../services/dataService'
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { createProject, getProjects, getQualifications, getWorkers, unasignWorker  } from '../services/dataService'
+
+const animatedComponents = makeAnimated();
 
 const Project = (project, active, extraProps) => {
     return(
@@ -74,13 +79,100 @@ const greenQuals = (project) => {
     return greenQuals;
 }
 
+const qualsDescriptions = (quals) => {
+    const qualOptions = [];
+
+    for (let i = 0; i < quals.length; i++) {
+        qualOptions.push({value: quals[i].description, label: quals[i].description});
+    }
+
+    return qualOptions;
+}
+
 const CreateProjectForm = (props) => { 
+    const { setProjects } = props
+    const [quals, setQualifications] = useState([])
+    const [selectedQuals, setSelectedQuals] = useState(null);
+    const [selectedSize, setSelectedSize] = useState(null);
+
+    const handleQualsChange = (selectedOptions) => {
+        setSelectedQuals(Array.isArray(selectedOptions) ? selectedOptions : []);
+    };
+
+    const handleSizeChange = (selectedOption) => {
+        setSelectedSize(selectedOption);
+    };
+
+    useEffect(() => {
+        getQualifications().then((quals) => { 
+            setQualifications(quals)
+        })
+    }, [])
     
+    return (
+        <div className="card">
+            <div className="card-body">
+                <form>
+                    <div className="form-group">
+                        <input type="text" className="form-control" id="name" placeholder="Enter the project name..." required/><br/>
+
+                        <Select
+                            id="quals"
+                            placeholder="Select the qualifications..."
+                            options={qualsDescriptions(quals)}
+                            value={selectedQuals}
+                            onChange={handleQualsChange}
+                            isSearchable
+                            isMulti
+                            closeMenuOnSelect={false}
+                            components={animatedComponents}
+                            required
+                        /><br/>
+
+                        <Select 
+                            id="size"
+                            placeholder="Select the project size..."
+                            options={[
+                                { key:'SMALL', value:'SMALL', label: 'SMALL' },
+                                { key:'MEDIM', value: 'MEDIUM', label: 'MEDIUM' },
+                                { value: 'BIG', label: 'BIG' }
+                            ]}
+                            value={selectedSize}
+                            isSearchable
+                            onChange={handleSizeChange} 
+                            required
+                        /><br/>
+
+                        <button type="button" className="btn btn-outline-primary"
+                            onClick={() => {
+                                const name = document.getElementById('name').value
+                                const size = selectedSize.value
+                                const quals = selectedQuals.map(x=>x.value)
+                                if (name && quals && size) {
+                                    createProject(name, quals, size).then(response => {
+                                        if (response?.data === 'OK') {
+                                            setSelectedQuals([])
+                                            setSelectedSize('')
+                                            document.getElementById('name').value = ''
+                                        } else { 
+                                            alert('Error: ' + response?.data)
+                                        }
+                                    }).then(() => { 
+                                        Promise.all([getProjects(), getWorkers()]).then(setProjects())
+                                    })
+                                }
+                            }}>
+                        Create Project</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 const Projects = () => {
     const [unassignDropdownOpen, setUnassignDropdownOpen] = useState(false);
-    const [data, setData] = useState([])
+    const [data] = useState([])
     // Add this to extraProps and then use to toggle the assign button
     // [assignDropdownOpen, setAssignDropdownOpen] = useState(false);
     const [projects, setprojects] = useState([])
