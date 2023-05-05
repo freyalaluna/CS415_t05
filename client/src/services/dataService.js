66e0b76469ee09a3487809d804939dada064cf74
nchaffee0 <nchaffee@colostate.edu>
2023-05-04 19:37:33 -0600

    finish project dataservice

--- client/src/services/dataService.js
+++ client/src/services/dataService.js
@@ -40,15 +40,16 @@ export function assignWorker(worker, project){
     return axios.put(SERVER_ADDRESS + 'assign', {worker: worker, project: project})
 }
 
-export function unasignWorker(worker, project) {
-    return axios.put(SERVER_ADDRESS + 'unassign' , { worker:worker, project:project })
-
 export function createProject(name, quals, size) {
     return axios.post(SERVER_ADDRESS + 'projects/' + name, { name: name, qualifications: quals, size: size })
 }
 
 export function startProject(name){
-    return axios.put(SERVER_ADDRESS + 'start' , { name: name})
+    return axios.put(SERVER_ADDRESS + 'start' , { name: name })
+}
+
+export function finishProject(name){
+    return axios.put(SERVER_ADDRESS + 'finish' , { name: name })
 }
 
 // export function createProject(name, quals, size) {
